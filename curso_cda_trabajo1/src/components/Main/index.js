c7b5f9fd37739f73373aac305c8edb003ab1768f
Sebastian Lezama <sebastian.lezama@gmail.com>
2022-07-15 19:18:34 -0300

    Fiddled with css

--- curso_cda_trabajo1/src/components/Main/index.js
+++ curso_cda_trabajo1/src/components/Main/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import image from '../../assets/trabajo_1.png'
+import './index.css'
 
 
 function Main() {
   return (
-    <div>
+    <div className='image-main'>
       <img src={image}></img>
     </div>
   );
