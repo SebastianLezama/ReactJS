1d5afee4bc771e3f57e03aba1cb80a7fb3a4661f
Sebastian Lezama <sebastian.lezama@gmail.com>
2022-07-23 17:45:27 -0300

    Trabajo calse 6 added header mod

--- curso_cda_trabajo1/src/components/NavBar/NavBar.js
+++ curso_cda_trabajo1/src/components/NavBar/NavBar.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import style from './NavBar.module.scss';
 import { BsGithub, BsCart } from 'react-icons/bs'
 import { AiFillLinkedin } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, NavItem, Collapse, NavbarToggler, Navbar, Nav, UncontrolledDropdown } from 'reactstrap';
 import DropDown from '../Header/DropDown';
 
 
@@ -15,7 +14,7 @@ function NavBar({ isInHeader }) {
     <nav className={isInHeader ? style.nav : style.navFooter}>
       <Link className='home' to='/'><h2>e-Commerce CDA</h2></Link>
       <ul>
-        <DropDown>Categorias</DropDown>
+        { isInHeader ? <DropDown>Categorias</DropDown> : null }
         <li>
           { isInHeader ? <Link exact="true" className="productos" 
             to="/productos">Productos</Link> : <a target="_blank"
