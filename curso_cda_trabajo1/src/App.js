996d7c556af3e38386152a76616ebb8dcd14f030
Sebastian Lezama <sebastian.lezama@gmail.com>
2022-08-02 23:51:39 -0300

    Trabajo clase 9, added responsive

--- curso_cda_trabajo1/src/App.js
+++ curso_cda_trabajo1/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout/Layout';
 import Cart from './components/Cart/Cart';
 import CardContainer from './components/Productos/Cards/CardContainer';
+import Favorites from './components/Productos/Favorites';
 
 
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/productos" element={<CardContainer />}/>
           <Route path="/category/:categoryId" element={<CardContainer />}/>
           <Route path="/cart" element={<Cart />}/>
+          <Route path="/favs" element={<Favorites />}/>
         </Route>
       </Routes>
     </>
--- curso_cda_trabajo1/src/components/NavBar/NavBar.js
+++ curso_cda_trabajo1/src/components/NavBar/NavBar.js
@@ -6,12 +6,15 @@ import { Link } from 'react-router-dom'
 import DropDown from '../Header/DropDown';
 import { FaBars, FaTimes } from 'react-icons/fa'
 import { CartContext } from '../../context/CartContext';
+import FavDrop from '../Header/FavDrop';
 
 
 function NavBar({ isInHeader }) {
   const { cartItems } = useContext(CartContext);
 
   const [ drop, setDrop ] = useState(false);
+  const [ dropFav, setDropFav ] = useState(false);
+
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
@@ -23,49 +26,99 @@ function NavBar({ isInHeader }) {
       else {setDrop(true)}
   }
   const onMouseLeave = () => {setDrop(false)}
+  
+  const onEnter = () => {
+    if(window.innerWidth < 960){setDropFav(false)}
+      else {setDropFav(true)}
+  }
+  const onLeave = () => {setDropFav(false)}
 
-
-  useEffect(() => {
-    if(window.innerWidth < 960){
-      setIcon(true)
-    } else setIcon(false)
-
-  }, [window.innerWidth])
+  // useEffect(() => {
+  //   if(window.innerWidth < 960){
+  //     setIcon(true)
+  //   } else setIcon(false)
+  // }, [window.innerWidth])
   
   const ShowIcon = () => {
       return (
-        click ? <FaTimes onClick={closeMobileMenu}/> : <FaBars />
+          <div className={style.bars}>
+            {click ? 
+            <FaTimes size={30} onClick={closeMobileMenu} /> 
+              :
+            <FaBars size={30} onClick={handleClick} />}
+          </div>
         )
   }
   
-  const bars = icon ? <ShowIcon /> : 'Productos';
-  // const navMenu = click ? style.navMenuActive : style.navMenu;
+  const Favoritos = () => {
+    return (
+      <li className={click ? style.navLinksMobile : style.navItem}
+        onMouseEnter={onEnter}
+        onMouseLeave={onLeave}
+        onClick={closeMobileMenu}
+      >
+        <Link to='/favs'>
+          <div className={style.navFav}>
+            <h2>Favoritos</h2>
+            {dropFav && <FavDrop />}
+          </div>
+        </Link>
+      </li>
+    )
+  }
+
 
   return (
     <nav className={isInHeader ? style.nav : style.navFooter}>
       <Link className={style.navLinks} to='/'><h2>e-Commerce CDA</h2></Link>
-      <ul className={style.navMenu}>
-        <li className={style.navItem}>
-          { isInHeader ? <Link exact="true" to="/cart" className={style.cart}> 
-            <BsCart  color='white' size={35} /> <span>{cartItems}</span></Link> :  <a className={style.linkedIn} target="_blank"
-            rel="noreferrer" href='https://www.linkedin.com/in/sebastian-lezama-89a7851b2/' >
-            <AiFillLinkedin  size={40} /> </a>}
+      <ShowIcon className={style.menuIcon} />
+      <ul className={click ? style.navMenuActive : style.navMenu}>
+        {isInHeader && <Favoritos />}
+        <li className={click ? style.navLinksMobile : style.navItem}
+          to='/cart'
+        >
+          { isInHeader ?
+            <Link 
+              exact="true" 
+              to="/cart" 
+            > 
+              <div className={style.cart} onClick={closeMobileMenu}>
+                <BsCart  color='white' size={35} />
+                <span>{cartItems}</span>
+              </div>
+            </Link>
+            : <a className={style.linkedIn} 
+                  target="_blank"
+                  rel="noreferrer" 
+                  href='https://www.linkedin.com/in/sebastian-lezama-89a7851b2/' 
+                >
+                <AiFillLinkedin  size={40} />
+              </a>}
         </li>
-        <li className={style.navItem}
+        <li className={click ? style.navLinksMobile : style.navItem}
             onMouseEnter={onMouseEnter}
             onMouseLeave={onMouseLeave}
           >
           { isInHeader ? 
-            <h2 onClick={handleClick} className={style.productos}>
-              <Link exact="true" 
-                to="/productos" className={style.navProd}><h2>{bars}</h2></Link>
+            <Link
+              as='li'
+              exact="true" 
+              to="/productos" 
+              className={style.navProd}
+              onClick={closeMobileMenu}
+            >
+              <div className={style.productos} >
+                <h2>Productos</h2>
                 {drop && <DropDown />}
-                
-            </h2> : <a className={style.github} target="_blank"
-              rel="noreferrer" href='https://github.com/SebastianLezama/'>
-              <BsGithub size={35} /> </a>}
+              </div> 
+            </Link>
+            : <a className={style.github} target="_blank"
+                rel="noreferrer" href='https://github.com/SebastianLezama/'
+              >
+                <BsGithub size={35} /> 
+              </a>
+              }
         </li>
-        
       </ul>
     </nav>
   )
