bda56004f09451476426e441ca817e9696e70f31
Sebastian Lezama <sebastian.lezama@gmail.com>
2023-03-08 13:39:16 -0300

    calendar library

--- agustinaApp/mente_app/src/App.jsx
+++ agustinaApp/mente_app/src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+// import "@fullcalendar/core/main.css";
+// import "@fullcalendar/daygrid/main.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const events = [{ title: "Agus", date: new Date() }];
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="Calendar">
+      <FullCalendar
+        defaultView="timeGridWeek"
+        plugins={[timeGridPlugin]}
+        events={events}
+        height="100%"
+        width="100%"
+        weekends={false}
+        slotMinTime="08:00:00"
+        slotMaxTime="21:00:00"
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
