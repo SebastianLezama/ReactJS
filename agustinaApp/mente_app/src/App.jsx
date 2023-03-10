9668113951cc9ae2398053b1e2c72786721038d6
Sebastian Lezama <sebastian.lezama@gmail.com>
2023-03-10 19:46:47 -0300

    Working response from gapi

--- agustinaApp/mente_app/src/App.jsx
+++ agustinaApp/mente_app/src/App.jsx
@@ -7,24 +7,24 @@ import { gapi } from "gapi-script";
 // import ApiCalendar from "react-google-calendar-api";
 
 function App() {
-  const [events, setEvents] = useState([]);
-
-  // const events = [{ title: "Agus", date: new Date() }];
+  const [events, setEvents] = useState([{ title: null, date: null }]);
+  // TODO make function for calendar events out of larger event arrays
 
   const CALENDAR_ID = import.meta.env.VITE_CALENDAR_ID;
   const API_KEY = import.meta.env.VITE_G_API_KEY;
-  const DISCOVERY_DOCS = [
-    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
-  ];
-  const SCOPE = "https://www.googleapis.com/auth/calendar.events";
+  // const DISCOVERY_DOCS = [
+  //   "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+  // ];
+  // const SCOPE = "https://www.googleapis.com/auth/calendar.events";
 
+  // TODO make events with larger info in the array
   const getEventsFromCalendar = async (calendarID, apiKey) => {
     async function initiate() {
       await gapi.client
         .init({
           apiKey: apiKey,
         })
-        .then(async function() {
+        .then(async function () {
           return await gapi.client.request({
             path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events/`,
             timeMin: new Date().toISOString(),
@@ -33,34 +33,19 @@ function App() {
             maxResults: 33,
             orderBy: "startTime",
           });
-          //   return await gapi.client.calendar.events.list({
-          //     calendarId: CALENDAR_ID,
-          //     timeMin: new Date().toISOString(),
-          //     showDeleted: false,
-          //     singleEvents: true,
-          //     orderBy: "startTime",
-          //   });
-          // })
-          // .then((response) => {
-          //   let events = response.result.items;
-          //   console.log(events);
         })
         .then(
           (response) => {
-            let events = response.result.items;
-            events.map((e) => {
-              e
-                ? {
-                    title: e.attendees,
-                    date: e.start.DayTime,
-                  }
-                : e;
-            });
+            console.log(response.result.items);
+            const events = response.result.items.map((e) => ({
+              title: e.summary,
+              date: e.start.dateTime,
+            }));
 
             console.log(events);
             setEvents(events);
           },
-          function(err) {
+          function (err) {
             return [false, err];
           }
         );
@@ -70,7 +55,6 @@ function App() {
 
   useEffect(() => {
     const events = getEventsFromCalendar(CALENDAR_ID, API_KEY);
-    // console.log(events);
     setEvents(events);
   }, []);
 
